a4a3079a9788693b60be9adcb27dc685225bb843
Ramon Jose Silva <ramon.jose@allink.com.br>
2024-06-12 11:05:53 -0300

    Added properties, controllers, dtos and responses

--- src/main/java/br/dev/jaysofthouse/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/dev/jaysofthouse/pontointeligente/api/PontoInteligenteApplication.java
@@ -2,8 +2,11 @@ package br.dev.jaysofthouse.pontointeligente.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+// Passing parameter exclude, will remove from application when it's running
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
